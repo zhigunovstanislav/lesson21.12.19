ffdebc614fe2d7c4217a8619c4b5c95732ba7fed
zhigunovstanislav <59096281+zhigunovstanislav@users.noreply.github.com>
2019-12-22 18:48:39 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+# задание 1
+name = 'Stas'
+age = 18
+temperature = 36.6
+print(f'Имя: {name},возраст: {age},Температура: {temperature}')
+surename = input('Напишите вашу фамилию: ')
+weithe = int(input('Какой ваш вес? '))
+print(surename,weithe)
+
